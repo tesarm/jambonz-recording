553cde9711b4eef8329742bfdc37e0a420078a8d
Michal Tesar <tesarm@gmail.com>
2021-05-26 17:06:27 +0100

    saving raw files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const config = require('./config');
+
 const loggerOpts = {
   timestamp: () => `, "time": "${new Date().toISOString()}"`,
   level: config.logLevel
@@ -6,17 +7,20 @@ const loggerOpts = {
 const logger = require('pino')(loggerOpts);
 const express = require('express');
 const cors = require('cors');
+const expressWs = require('express-ws');
 
 const routes = require('./lib/routes/index');
+const ws = require('./lib/ws/handler');
 
 const app = express();
+expressWs(app);
+
 app.use(cors());
 app.use('/', routes);
+app.ws('/', ws);
 app.use((err, req, res, next) => {
   logger.error(err, 'burped error');
   res.status(err.status || 500).json({msg: err.message});
 });
-app.get('/', (req, res) => res.send('hello world'));
-
 
 app.listen(config.httpPort, () => logger.info(`listening for HTTP traffic on port ${config.httpPort}`));
--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 
 require('dotenv').config();
 
+const assert = require('assert');
+assert.ok(process.env.JAMBONES_RECFOLDER, 'missing JAMBONES_RECFOLDER env var');
+
 module.exports = {
   httpPort: process.env.HTTP_PORT || 3000,
   logLevel: process.env.JAMBONES_LOGLEVEL || 'info',
+
+  recFolder: process.env.JAMBONES_REC_FOLDER
 };
